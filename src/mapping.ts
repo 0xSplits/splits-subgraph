ee69421e0edc44335c217fd98bafe3f91c5969af
William Minshew <wminshew@gmail.com>
2022-02-25 15:12:12 -0800

    Initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,80 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  SplitMain,
+  CancelControlTransfer,
+  ControlTransfer,
+  CreateSplit,
+  DistributeERC20,
+  DistributeETH,
+  InitiateControlTransfer,
+  UpdateSplit,
+  Withdrawal
+} from "../generated/SplitMain/SplitMain"
+import { ExampleEntity } from "../generated/schema"
+
+export function handleCancelControlTransfer(
+  event: CancelControlTransfer
+): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.split = event.params.split
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.PERCENTAGE_SCALE(...)
+  // - contract.createSplit(...)
+  // - contract.getController(...)
+  // - contract.getERC20Balance(...)
+  // - contract.getETHBalance(...)
+  // - contract.getHash(...)
+  // - contract.getNewPotentialController(...)
+  // - contract.predictImmutableSplitAddress(...)
+  // - contract.walletImplementation(...)
+}
+
+export function handleControlTransfer(event: ControlTransfer): void {}
+
+export function handleCreateSplit(event: CreateSplit): void {}
+
+export function handleDistributeERC20(event: DistributeERC20): void {}
+
+export function handleDistributeETH(event: DistributeETH): void {}
+
+export function handleInitiateControlTransfer(
+  event: InitiateControlTransfer
+): void {}
+
+export function handleUpdateSplit(event: UpdateSplit): void {}
+
+export function handleWithdrawal(event: Withdrawal): void {}
