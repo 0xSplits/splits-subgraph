0366368bf863a731bd0141fe4a9f1626302a2e82
Mike H <mikeh09@gmail.com>
2023-04-25 14:48:13 -0700

    handle create diversifier

--- src/templates.mapping.ts
+++ src/templates.mapping.ts
@@ -1,10 +1,12 @@
 import { CreateRecoup } from "../generated/Recoup/Recoup";
+import { CreateDiversifier } from "../generated/DiversifierFactory/DiversifierFactory";
 import {
   WaterfallTranche,
   Split,
 } from "../generated/schema";
 import {
   createJointId,
+  getPassThroughWallet,
   getWaterfallModule,
 } from "./helpers";
 
@@ -34,4 +36,17 @@ export function handleCreateRecoup(event: CreateRecoup): void {
       split.save();
     }
   }
+}
+
+export function handleCreateDiversifier(event: CreateDiversifier): void {
+  let passThroughWalletId = event.params.diversifier.toHexString();
+  let passThroughWallet = getPassThroughWallet(passThroughWalletId);
+  if (!passThroughWallet) return;
+
+  passThroughWallet.parentEntityType = 'diversifier';
+  passThroughWallet.save();
+
+  // Need to update parentEntityType on split and swappers as well, but
+  // need to handle that in SetPassThrough since the passThroughAccount
+  // is set to address(0) on creation
 }
